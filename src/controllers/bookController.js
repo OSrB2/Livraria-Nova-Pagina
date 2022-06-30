15130a8c34fd7e45b885ba5554e0748894a0bb44
Pedro Oliveira <pedro_oliva@outlook.com.br>
2022-06-30 00:44:25 -0300

    chore: updating

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,20 +2,21 @@ const { Books } = require('../database/models');
 const cloudinary = require('../config/cloudinary');
 const fs = require('fs');
 
-const imagesFolder = 'images/';
+const imageFolder = 'images/';
 
 const BookController = {
   async create(req, res) {
     const file = req.files[0];
-    const uploadPath = await cloudinary.uploads(file.path, 'livraria');
+    const uploadPath = await cloudinary.uploads(file.path, 'Books');
     fs.unlinkSync(file.path);
 
     const newBook = await Books.create({
       ...req.body,
-      // Forma de upload na aplicação.
-      // images: imagesFolder + file.filename,
+      // forma de upload na aplicacao
+      // image: imageFolder + file.filename
       images: uploadPath.imageUrl,
     });
+
     return res.status(201).json({ dados: newBook });
   },
 };
